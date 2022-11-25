660cbee4328f675dbc77c1125ec4e6e8a1fc2b62
Divyansh62 <64961736+Divyansh62@users.noreply.github.com>
2022-11-25 17:03:35 +0530

    FileUpdate1

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -11,7 +11,7 @@ API.interceptors.request.use((req)=>{
 
 // https://moment-internship.herokuapp.com/posts  divyansh
 export const fetchPosts= (page)=> API.get(`/posts?page=${page}`);
-export const fetchPostsBySearch = (searchQuery) => API.get(`/posts/search?searchQuery=${searchQuery.search || 'none'}&tags=${searchQuery.tags}`);
+export const fetchPostsBySearch = (searchQuery) => API.get(`/posts/search?searchQuery=${searchQuery.search || 'none'}&searchYear=${searchQuery.searchyear||'none'}&tags=${searchQuery.tags}`);
 export const createPost=(newPost)=>API.post('/posts',newPost);
 export const likePost = (id) => API.patch(`/posts/${id}/likePost`);
 export const updatePost=(id, updatedPost)=>API.patch(`/posts/${id}`,updatedPost);
